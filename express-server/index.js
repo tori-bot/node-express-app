f7abc80cab25fa0b15f063437362dbc217bc880f
victoria makena <makenavictoria1@gmail.com>
2022-08-03 11:01:58 +0300

    setup static files

--- express-server/index.js
+++ express-server/index.js
@@ -1,12 +1,24 @@
+//module to generate absolute paths to serve static assets(js,html,css,images)
+const path = require('path');
+
 const express = require('express');
 //call express function to create new app
 const app = express();
 
-//creae routes and liste on a specific port
-app.get('/', (req, res) => {
-    res.send({ message: 'Hello World!' });
-});
+//path.join takes 2 arguements: cwd, secondary dir
+app.use(express.static(path.join(__dirname, 'public')));
+
+//create routes and liste on a specific port
+
+// app.get('/', (req, res) => {
+//     res.send({ message: 'Hello World!' });
+// });
 //when a person makes a GET request to your homepage, localhost:3333, the arrow function is called and will display "Hello World!"
+
+app.get('/', (req, res) => {
+     res.sendFile(`${__dirname}/public/index.html`);
+ });
+
 
 //setup a listener
 app.listen(3333, () => {
